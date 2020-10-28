6f3a8db1f31196be3afdf2d5e4d7d071abef3c65
Evgeny Volovlikov <volovlikovevgeny@mail.ru>
2020-10-28 17:55:34 +0300

    removing particles

--- src/App.jsx
+++ src/App.jsx
@@ -14,27 +14,18 @@ const Contacts = lazy(() => import('./components/contact/contact.component'));
 const Footer = lazy(() => import('./components/footer/footer.component'));
 const ScrollBtn = lazy(() => import('./components/scroll-up-btn/scroll-up-btn'));
 
-const Particles = lazy(() => import('react-particles-js'));
 
 
-
-const particleOptions = {
-  particles: {
-    number: {
-      value: 15,
-      density: {
-        enable: true,
-        value_area: 1000
-      }
-    }
-  }
-}
 
 const App = () => (
   <div>
-    <Suspense fallback={<div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '2rem' }}>Loading</div>}>
-      <Particles
-        params={particleOptions} className='particles' />
+    <Suspense fallback={<div style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontSize: '2rem'
+    }}>Loading</div>}>
       <Navigation />
       <Home />
       <About />
